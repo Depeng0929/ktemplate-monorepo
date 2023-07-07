097a544201a11a82b4f18be7bd1361a5444ab8a7
depeng.kong <15365182986@163.com>
2023-07-07 17:46:12 +0800

    fix

---
+++ packages/config/src/is.ts
@@ -0,0 +1,2 @@
+export const isDev = import.meta.env.VITE_APP_ENV === 'development'
+export const isProd = import.meta.env.VITE_APP_ENV === 'production'
---
+++ packages/config/src/url.ts
@@ -0,0 +1,1 @@
+export const apiBase = import.meta.env.VITE_APP_API
